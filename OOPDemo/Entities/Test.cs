fc7b2378f41c83c6a7bec8959630e008a47f1c43
Ali Alper Yalcin <alialper.yalcin@student.uhasselt.be>
2022-02-22 18:45:25 +0100

    Commit 1

---
+++ OOPDemo/Entities/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo.Entities
+{
+    class Test
+    {
+        int Id;
+        string Name;
+
+    }
+}
